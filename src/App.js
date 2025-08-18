be6495d843ff995e8ec59c39cfe52992e5c551d0
Aliipou <161300611+Aliipou@users.noreply.github.com>
2025-08-18 23:47:45 +0300

    Initial commit: MovieWizard personalized platform

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import SBSB from "./components/SBSB.jsx";
+import React from "react";
+import RatingTab from "./components/RatingTab.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <SBSB />
+      <RatingTab />
+    </div>
+  );
+};
+export default App;
